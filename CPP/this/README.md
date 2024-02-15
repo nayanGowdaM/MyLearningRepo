5ea28a842f567f5ad356c09eb59334ee6843b269
nayanGowdaM <mnayangowda@gmail.com>
2024-02-15 14:29:42 +0530

    This keyword

---
+++ CPP/this/README.md
@@ -0,0 +1,12 @@
+## this Keyword
+it is the pointer to the current object instance that the method belongs to 
+Data type: 
+in a non const function 
+```cpp
+Entity* e=this;
+```
+
+in a const function 
+```cpp 
+const Entity* e=this
+```
---
+++ CPP/this/main.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Entity {
+        int x, y;
+    public:
+        Entity(int x, int y){
+            // Entity* e=this  equivalence 
+            this->x=x;
+            this->y=y;
+        }
+};
